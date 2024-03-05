3a9b7c1ca75e724c1b32ab33793975ab6b9750df
Matthew Tran <matt.h.tran@gmail.com>
2024-03-05 15:33:56 -0500

    Proper image path

--- my-app/src/Footer.jsx
+++ my-app/src/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <footer className="w-full bg-gray-800 p-4">
     <div className="flex justify-end">
         <a href="https://www.linkedin.com/in/matthew-d-tran" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
-          <img src="linkedin.png" alt="LinkedIn" style={{width: "2em", height: "2em" }}/>
+          <img src="/linkedin.png" alt="LinkedIn" style={{width: "2em", height: "2em" }}/>
         </a>
         <a href="https://github.com/tran-matt" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
-        <img src="path-to-your-github-image.png" alt="GitHub" style={{ width: '2em', height: '2em' }} />
+        <img src="/github.png" alt="GitHub" style={{ width: '2em', height: '2em' }} />
         </a>
       </div>
     </footer>
