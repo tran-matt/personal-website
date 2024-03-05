3c69aa205d0f738c927f893705c7bc664ceae2fb
Matthew Tran <matt.h.tran@gmail.com>
2024-03-05 15:42:37 -0500

    Added proper dependencies in my-app

--- my-app/src/Footer.jsx
+++ my-app/src/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="w-full bg-gray-800 p-4">
     <div className="flex justify-end">
         <a href="https://www.linkedin.com/in/matthew-d-tran" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
-          <img src="/linkedin.png" alt="LinkedIn" style={{width: "2em", height: "2em" }}/>
+          <FaLinkedin size="2em" />
         </a>
         <a href="https://github.com/tran-matt" target="_blank" rel="noopener noreferrer" className="text-white mx-2">
-        <img src="/github.png" alt="GitHub" style={{ width: '2em', height: '2em' }} />
+          <FaGithub.png size="2em" />
         </a>
       </div>
     </footer>
